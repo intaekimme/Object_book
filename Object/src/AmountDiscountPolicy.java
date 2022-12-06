12e355edf8ba00ce4d13ff18d7c4bb163d741225
InTae <kit938639@gmail.com>
2022-12-06 23:29:33 +0900

    :sparkles: feat : AmountDiscountPolish extends DiscountPolish

---
+++ Object/src/AmountDiscountPolicy.java
@@ -0,0 +1,13 @@
+public class AmountDiscountPolicy extends DiscountPolicy{
+    private Money discountAmount;
+
+    public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+        super(conditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money getDiscountAmounts(Screening screening) {
+        return discountAmount;
+    }
+}

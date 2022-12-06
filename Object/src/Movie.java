cd98f66a919bad946053e31c07245978962c4589
InTae <kit938639@gmail.com>
2022-12-07 01:22:50 +0900

    :sparkles: feat : Movie.calculateMovieFee 할인 정책이 없는 경우 - 할인 정책이 없는 경우 예외 케이스로 취급 - 책임이 DiscountPolicy 에서 - Movie 로 이동 - 책임의 위치를 결정하기 위해 조건문을 사용하는 좋지 못한 설계의 예시

--- Object/src/Movie.java
+++ Object/src/Movie.java
@@ -18,6 +18,10 @@ public class Movie {
     }
 
     public Money calculateMovieFee(Screening screening) {
+        if(discountPolicy == null){
+            return fee;
+        }
+
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 }
